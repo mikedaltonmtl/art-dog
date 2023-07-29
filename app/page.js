45a6a982bff315f68fa9a8d650be0eb22f7e2925
mike-dalton <michael.j.dalton@gmail.com>
2023-07-29 12:25:39 -0400

    add and import first batch of images

--- app/page.js
+++ app/page.js
@@ -1,5 +1,24 @@
-import Image from 'next/image';
 import styles from './page.module.css';
+import Image from 'next/image';
+
+import romy001 from '../public/Romy001.jpg';
+import romy002 from '../public/Romy002.jpg';
+import romy003 from '../public/Romy003.jpg';
+import romy004 from '../public/Romy004.jpg';
+import romy005 from '../public/Romy005.jpg';
+import romy006 from '../public/Romy006.jpg';
+import romy007 from '../public/romy007.jpg';
+
+const images = [
+  { name: romy001, desc: 'Dog by the fireplace' },
+  { name: romy002, desc: 'Dog' },
+  { name: romy003, desc: 'Dog' },
+  { name: romy004, desc: 'Dog' },
+  { name: romy005, desc: 'Dog' },
+  { name: romy006, desc: 'Dog' },
+  { name: romy007, desc: 'Dog' },
+];
+
 
 export default function Home() {
   return (
@@ -9,6 +28,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
+        {/* <Image src={romy001} alt="Dog by the fireplace" height={300} placeholder="blur" /> */}
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
