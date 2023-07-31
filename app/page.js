9bc3c4da7f1934a328a9668fc5f2c9710357f689
mike-dalton <michael.j.dalton@gmail.com>
2023-07-31 14:56:36 -0400

    the end.... not a great result :(

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
-import styles from './page.module.css';
+'use client';
+
+import { useRef } from 'react';
 import Image from 'next/image';
 
 import romy001 from '../public/Romy001.jpg';
@@ -7,108 +9,81 @@ import romy003 from '../public/Romy003.jpg';
 import romy004 from '../public/Romy004.jpg';
 import romy005 from '../public/Romy005.jpg';
 import romy006 from '../public/Romy006.jpg';
-import romy007 from '../public/romy007.jpg';
+import romy007 from '../public/Romy007.jpg';
 
-const images = [
-  { name: romy001, desc: 'Dog by the fireplace' },
-  { name: romy002, desc: 'Dog' },
-  { name: romy003, desc: 'Dog' },
-  { name: romy004, desc: 'Dog' },
-  { name: romy005, desc: 'Dog' },
-  { name: romy006, desc: 'Dog' },
-  { name: romy007, desc: 'Dog' },
+const importedImages = [
+  { id: 'romy001', image: romy001, desc: 'Dog by the fireplace' },
+  { id: 'romy002', image: romy002, desc: 'Dog on rug outside' },
+  { id: 'romy003', image: romy003, desc: "Dog on Alice's lap" },
+  { id: 'romy004', image: romy004, desc: 'Dog with Clara' },
+  { id: 'romy005', image: romy005, desc: 'Dog outside under bush' },
+  { id: 'romy006', image: romy006, desc: 'Dog inside looking playful' },
+  { id: 'romy007', image: romy007, desc: 'Dog in car with Alice' },
 ];
 
-
 export default function Home() {
+
+  const images = importedImages.map(image => {
+    return (
+      <Image
+        key={image.id}
+        className="image"
+        src={image.image}
+        alt={image.desc}
+        placeholder="blur"
+        draggable="false"
+      />
+    );
+  });
+
+  const track = useRef(null);
+
+  const handleOnDown = e => track.current.dataset.mouseDownAt = e.clientX;
+
+  const handleOnUp = () => {
+    track.current.dataset.mouseDownAt = "0";
+    track.current.dataset.prevPercentage = track.current.dataset.percentage;
+  };
+  
+  const handleOnMove = e => {
+    if (track.current.dataset.mouseDownAt === "0") return;
+    
+    const mouseDelta = parseFloat(track.current.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
+    
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentageUnconstrained = parseFloat(track.current.dataset.prevPercentage) + percentage;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    
+    track.current.dataset.percentage = nextPercentage;
+    
+    track.current.animate({
+      transform: `translate(${nextPercentage}%, -50%)`
+    }, { duration: 1200, fill: "forwards" });
+    
+    for (const image of track.current.getElementsByClassName("image")) {
+      image.animate({
+        objectPosition: `${100 + nextPercentage}% center`
+      }, { duration: 1200, fill: "forwards" });
+    }
+  };
+  
+  /* -- Had to add extra lines for touch events -- */
+  window.onmousedown = e => handleOnDown(e);
+  window.ontouchstart = e => handleOnDown(e.touches[0]);
+  window.onmouseup = e => handleOnUp(e);
+  window.ontouchend = e => handleOnUp(e.touches[0]);
+  window.onmousemove = e => handleOnMove(e);
+  window.ontouchmove = e => handleOnMove(e.touches[0]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        {/* <Image src={romy001} alt="Dog by the fireplace" height={300} placeholder="blur" /> */}
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div
+        id="image-track"
+        ref={track}
+        data-mouse-down-at="0"
+        data-prev-percentage="0"
+      >
+        {images}
       </div>
     </main>
   );
